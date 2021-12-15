d3118128a1bbc7a771545e27587516f2fe56f7d7
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-16 02:07:42 +0530

    Addded: checked user password is valid or invalid

--- src/main/java/com/blz/userregistration/UserRegistration.java
+++ src/main/java/com/blz/userregistration/UserRegistration.java
@@ -2,19 +2,18 @@ package com.blz.userregistration;
 
 public class UserRegistration {
 
-    String VALIDATE_NAME= "^[A-Z]{1}[a-z]{2,}$";
-    String VALIDATE_EMAIL= "^[a-z0-9]{3,20}([_+.-][a-z0-9]+)?@[a-z0-9]+.[a-z]{2,3}(.[a-z]{2})?$";
-
-        public String validateFirstName (String firstName) {
-            if (firstName.matches(VALIDATE_NAME)) {
-                return "valid";
-            }
-            else {
-                return "Invalid";
-            }
+    String VALIDATE_NAME = "^[A-Z]{1}[a-z]{2,}$";
+    String VALIDATE_EMAIL = "^[a-z0-9]{3,20}([_+.-][a-z0-9]+)?@[a-z0-9]+.[a-z]{2,3}(.[a-z]{2})?$";
+    String  VALIDATE_PASSWORD_WITH_RULE_1= "^(?=.[a-zA-Z0-9]).{8,}$";
+    public String validateFirstName(String firstName) {
+        if (firstName.matches(VALIDATE_NAME)) {
+            return "valid";
+        } else {
+            return "Invalid";
         }
+    }
 
-    public String validateLastName (String lastName) {
+    public String validateLastName(String lastName) {
         if (lastName.matches(VALIDATE_NAME)) {
             return "valid";
         } else {
@@ -22,17 +21,24 @@ public class UserRegistration {
         }
     }
 
-      public String validateEmailID (String Email){
-            if (Email.matches(VALIDATE_EMAIL)) {
-                return "valid";
-            } else {
-                return "Invalid";
-            }
+    public String validateEmailID(String Email) {
+        if (Email.matches(VALIDATE_EMAIL)) {
+            return "valid";
+        } else {
+            return "Invalid";
         }
+    }
+    public String validatePassword(String Password){
+        if (Password.matches(VALIDATE_PASSWORD_WITH_RULE_1)){
+            return "valid";
+        }
+        else {
+            return "Invalid";
+        }
+    }
 
     public static void main(String[] args) {
-
-            System.out.println("Welcome to User Registration");
+        System.out.println("Welcome to User Registration");
     }
 }
 
--- src/test/java/com/blz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/blz/userregistration/UserRegistrationTest.java
@@ -4,68 +4,98 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
-       // User Register Fast Name
+    // User Register Fast Name
     @Test
-    public void whenGivenFirstName_ShouldHaveMinimum3CharWithCamelCase(){
-        UserRegistration validator=new UserRegistration();
+    public void whenGivenFirstName_ShouldHaveMinimum3CharWithCamelCase() {
+        UserRegistration validator = new UserRegistration();
         String firstName = validator.validateFirstName("Manisha");
-        Assert.assertEquals("valid" , firstName);
+        Assert.assertEquals("valid", firstName);
     }
 
     @Test
     public void whenGivenFirstName_WithSmallLetter() {
-        UserRegistration validator=new UserRegistration();
+        UserRegistration validator = new UserRegistration();
         String firstName = validator.validateFirstName("manisha");
-        Assert.assertEquals("Invalid" , firstName);
+        Assert.assertEquals("Invalid", firstName);
     }
 
     @Test
     public void whenGivenFirstName_WithOtherLetterCapital() {
-        UserRegistration validator=new UserRegistration();
+        UserRegistration validator = new UserRegistration();
         String firstName = validator.validateFirstName("mANisha");
-        Assert.assertEquals("Invalid" , firstName);
+        Assert.assertEquals("Invalid", firstName);
     }
 
     // User Register Last Name
     @Test
     public void whenGivenLastName_ShouldHaveMinimum3CharWithCamelCase() {
-        UserRegistration validator=new UserRegistration();
+        UserRegistration validator = new UserRegistration();
         String lastName = validator.validateFirstName("Kahar");
-        Assert.assertEquals("valid" , lastName);
+        Assert.assertEquals("valid", lastName);
     }
 
     @Test
     public void whenGivenLastName_WithSmallLetter() {
-        UserRegistration validator=new UserRegistration();
+        UserRegistration validator = new UserRegistration();
         String lastName = validator.validateLastName("kahar");
-        Assert.assertEquals("Invalid" , lastName);
+        Assert.assertEquals("Invalid", lastName);
     }
 
     @Test
     public void whenGivenLastName_WithOtherLetterCapital() {
-        UserRegistration validator=new UserRegistration();
+        UserRegistration validator = new UserRegistration();
         String lastName = validator.validateFirstName("KAhar");
-        Assert.assertEquals("Invalid" , lastName);
+        Assert.assertEquals("Invalid", lastName);
     }
-      // User Register Email-id
+
+    // User Register Email-id
     @Test
-    public void  whenGivenEmail_Valid(){
-        UserRegistration validator=new UserRegistration();
+    public void whenGivenEmail_Valid() {
+        UserRegistration validator = new UserRegistration();
         String Email = validator.validateEmailID("abc.xyz@bl.co.in");
-        Assert.assertEquals("valid",Email);
+        Assert.assertEquals("valid", Email);
     }
 
     @Test
-    public void whenGivenEmail_WithoutatSignShouldReturnInvalid(){
-        UserRegistration validator=new UserRegistration();
+    public void whenGivenEmail_WithoutatSignShouldReturnInvalid() {
+        UserRegistration validator = new UserRegistration();
         String Email = validator.validateEmailID("abc.xyz.bl.co.in");
-        Assert.assertEquals("Invalid",Email);
+        Assert.assertEquals("Invalid", Email);
     }
 
     @Test
-    public void whenGivenEmail_StartWithDotShouldReturnInvalid (){
-        UserRegistration validator=new UserRegistration();
+    public void whenGivenEmail_StartWithDotShouldReturnInvalid() {
+        UserRegistration validator = new UserRegistration();
         String Email = validator.validateEmailID(".1abc.xyz@bl.co.in");
-        Assert.assertEquals("Invalid",Email);
+        Assert.assertEquals("Invalid", Email);
+    }
+
+    // User Register Password
+    @Test
+    public void whenGivenPassword_Valid() {
+        UserRegistration validator = new UserRegistration();
+        String password = validator.validatePassword("Riya@123");
+        Assert.assertEquals("valid", password);
+    }
+
+    @Test
+    public void whenGivenPassword_LessThan8CharacterShouldReturnInValid() {
+        UserRegistration validator = new UserRegistration();
+        String password = validator.validatePassword("Raj@123");
+        Assert.assertEquals("Invalid", password);
+    }
+
+    @Test
+    public void whenGivenPassword_WithoutUpperCaseShouldReturnInValid (){
+        UserRegistration validator = new UserRegistration();
+        String password = validator.validatePassword("riya123");
+        Assert.assertEquals("Invalid", password);
+    }
+
+    @Test
+    public void whenGivenPassword_WithoutIntegerCaseShouldReturnInvalid() {
+        UserRegistration validator = new UserRegistration();
+        String password = validator.validatePassword("riya@hy");
+        Assert.assertEquals("Invalid", password);
     }
 }
